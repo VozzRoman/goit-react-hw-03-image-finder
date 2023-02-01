0217e94c4e8fac56e028fefdedf379c17ab40d4e
VozzRoman <looper0500@ukr.net>
2023-02-01 17:36:42 +0200

    adding

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,21 +1,33 @@
 import css from '../Searchbar/SearchbarStyle.module.css';
+import { Formik, Form } from 'formik';
+
+const initialValues = {
+  search: '123',
+};
 
 export const Searchbar = () => {
+  const handleSubmit = (value, actions) => {
+    console.log(value);
+    console.log(actions);
+  };
   return (
     <header className={css.Searchbar}>
-      <form className={css.SearchForm}>
-        <button type="submit" className={css.SearchFormButton}>
-          <span className={css.SearchFormButtonLabel}>Search</span>
-        </button>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={css.SearchForm}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
 
-        <input
-          className={css.SearchFormInput}
-          type="text"
-          //  autocomplete="off"
-          //  autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            name="search"
+            //  autocomplete="off"
+            //  autofocus
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Formik>
     </header>
   );
 };
