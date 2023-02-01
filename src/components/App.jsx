e009efebddd027b63e003177e4f0e923408969ce
VozzRoman <looper0500@ukr.net>
2023-02-01 19:32:24 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,16 @@ import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
+  state = {
+    pictureSearch: '',
+  };
+
+  handleFormSubmit = serchPicture => {
+    console.log(serchPicture);
+    this.setState({ pictureSearch: serchPicture });
+    console.log(this.state.pictureSearch);
+  };
   render() {
-    return <Searchbar />;
+    return <Searchbar onSubmit={this.handleFormSubmit} />;
   }
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,33 +1,49 @@
 import css from '../Searchbar/SearchbarStyle.module.css';
-import { Formik, Form } from 'formik';
 
-const initialValues = {
-  search: '123',
-};
+import { Component } from 'react';
 
-export const Searchbar = () => {
-  const handleSubmit = (value, actions) => {
-    console.log(value);
-    console.log(actions);
+export class Searchbar extends Component {
+  state = {
+    search: '',
   };
-  return (
-    <header className={css.Searchbar}>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form className={css.SearchForm}>
+
+  handleOnCange = e => {
+    this.setState({
+      search: e.currentTarget.value.toLowerCase(),
+    });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    if (this.state.search.trim() === '') {
+      alert('Введите имя коллекции');
+      return;
+    }
+    this.props.onSubmit(this.state.search);
+    this.setState({ search: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form onSubmit={this.handleOnSubmit} className={css.SearchForm}>
           <button type="submit" className={css.SearchFormButton}>
             <span className={css.SearchFormButtonLabel}>Search</span>
           </button>
 
           <input
+            onChange={this.handleOnCange}
+            value={this.state.search}
             className={css.SearchFormInput}
             type="text"
             name="search"
-            //  autocomplete="off"
+            autoComplete="off"
             //  autofocus
             placeholder="Search images and photos"
           />
-        </Form>
-      </Formik>
-    </header>
-  );
-};
+        </form>
+      </header>
+    );
+  }
+}
